eed6410fdd5c84eb7d155b21cab0ca9ee172421a
Roo-M <51594@edu.raseko.fi>
2025-04-02 22:55:18 +0300

    Lisätty dialogi tietoja ohjelmasta

---
+++ aboutDialog_ui.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'aboutDialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QSizePolicy,
+    QTextBrowser, QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(374, 262)
+        self.createdByLabel = QLabel(Dialog)
+        self.createdByLabel.setObjectName(u"createdByLabel")
+        self.createdByLabel.setGeometry(QRect(10, 10, 101, 16))
+        font = QFont()
+        font.setPointSize(10)
+        self.createdByLabel.setFont(font)
+        self.createdByTextBrowser = QTextBrowser(Dialog)
+        self.createdByTextBrowser.setObjectName(u"createdByTextBrowser")
+        self.createdByTextBrowser.setGeometry(QRect(10, 30, 351, 221))
+        font1 = QFont()
+        font1.setPointSize(11)
+        self.createdByTextBrowser.setFont(font1)
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.createdByLabel.setText(QCoreApplication.translate("Dialog", u"Ohjelman tekij\u00e4t", None))
+        self.createdByTextBrowser.setHtml(QCoreApplication.translate("Dialog", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt; font-weight:600; color:#00aaff;\">Sovelluksen tekij\u00e4t</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">T\u00e4m\u00e4n sovelluksen sinulle tuottivat:</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:16pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
+                        "margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600;\">Tivi20oa</span><span style=\" font-size:16pt;\">:n opiskelijat</span></p></body></html>", None))
+    # retranslateUi
+
--- administrative.py
+++ administrative.py
@@ -1,6 +1,5 @@
-# PYSIDE6-MALLI SOVELLUKSEN PÄÄIKKUNAN LUOMISEEN
-# KÄÄNNETYSTÄ KÄYTTÖLIITTYMÄTIEDOSTOSTA (administrative_ui.py)
-# =======================================================
+# HALLINTASOVELLUKSEN PÄÄIKKUNAN JA DIALOGIEN KOODI
+# =================================================
 
 # KIRJASTOJEN JA MODUULIEN LATAUKSET
 # ----------------------------------
@@ -12,8 +11,11 @@ import json # JSON-objektien ja tiedostojen käsittely
 
 # Asennuksen vaativat kirjastot
 from PySide6 import QtWidgets # Qt-vimpaimet
+
+# Käyttöliittymä, moduulien lataukset
 from administrative_ui import Ui_MainWindow # Käännetyn käyttöliittymän luokka
-from settingsDialog_ui import Ui_Dialog # Asetukset-dialogin luokka
+from settingsDialog_ui import Ui_Dialog as Settings_Dialog # Asetukset-dialogin luokka
+from aboutDialog_ui import Ui_Dialog as About_Dialog
 
 # Omat moduulit
 import cipher
@@ -40,19 +42,32 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Asetukset-valikon muokkaa toiminto avaa Asetukset-dialogi-ikkunan
         self.ui.actionMuokkaa.triggered.connect(self.openSettingsDialog)
+        self.ui.actionTietoja_ohjelmasta.triggered.connect(self.openAboutDialog)
 
 
 
     # OHJELMOIDUT SLOTIT
-    # ------------------
+    # ==================
 
+
+
+    # DIALOGIEN AVAUSMETODIT
+    # ----------------------
+
+    # Asetusdialogin avaus
     def openSettingsDialog(self):
-        self.saveSettingsDialog = SaveSettingsDialog()
+        self.saveSettingsDialog = SaveSettingsDialog() # Luodaan luokasta olio
         self.saveSettingsDialog.setWindowTitle('Palvelinasetukset')
-        self.saveSettingsDialog.exec()
+        self.saveSettingsDialog.exec() # Luodaan dialogille oma event loop
 
 
-    # Avataan MessageBox
+    # Tietoja ohjelmasta -dialogin avaus
+    def openAboutDialog(self):
+        self.aboutDialog = AboutDialog()
+        self.aboutDialog.setWindowTitle('Tietoja ohjelmasta')
+        self.aboutDialog.exec() # Luodaan dialogi event loop
+
+    # Malli mahdollista virheilmoitusta varten
     def openWarning(self):
         msgBox = QtWidgets.QMessageBox()
         msgBox.setIcon(QtWidgets.QMessageBox.Critical)
@@ -62,7 +77,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         msgBox.exec()
 
 # Asetusten tallennusikkunan luokka
-class SaveSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
+class SaveSettingsDialog(QtWidgets.QDialog, Settings_Dialog):
     """A class for creating objects to open settings dialog window"""
 
     # Määritellään olionmuodostin ja kutsutaan yliluokkien muodostimia
@@ -70,7 +85,7 @@ class SaveSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
         super().__init__()
 
         # Luodaan käyttöliittymä konvertoidun tiedoston perusteella MainWindow:n ui-ominaisuudeksi. Tämä suojaa lopun MainWindow-olion ylikirjoitukselta, kun ui-tiedostoa päivitetään
-        self.ui = Ui_Dialog()
+        self.ui = Settings_Dialog()
 
         # Kutsutaan käyttöliittymän muodostusmetodia setupUi
         self.ui.setupUi(self)
@@ -85,6 +100,7 @@ class SaveSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
         self.currentSettings = {}
 
         # Tarkistetaan ensin, että asetustiedosto on olemassa
+        # TODO: Yksinkertaista asetusten luku käyttämällä json.load-metodia
         try:
             with open('settings.json', 'rt') as settingsFile:
                 jsonData = settingsFile.read()
@@ -133,6 +149,7 @@ class SaveSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
         }
 
         # Muunnetaan sanakirja JSON-muotoon
+        # TODO: Yksinkertaista muuttamalla json.dump-metodia käyttäväksi
         jsonData = json.dumps(settingsDictionary)
 
         # Avataan asetustiedosto ja kirjoitetaan asetukset
@@ -147,10 +164,20 @@ class SaveSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
         msgBox.setWindowTitle('Luodaan uusi asetustiedosto')
         msgBox.setText('Syötä kaikkien kenttien tiedot!')
         msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
-        msgBox.exec()
+        msgBox.exec() # Luodaan Msg Box:lle oma event loop
 
+class AboutDialog(QtWidgets.QDialog, About_Dialog):
+    """A class to show About dialog."""
+    def __init__(self):
+        super().__init__()
+        # Luodaan käyttöliittymä konvertoidun tiedoston perusteella MainWindow:n ui-ominaisuudeksi. Tämä suojaa lopun MainWindow-olion ylikirjoitukselta, kun ui-tiedostoa päivitetään
+        self.ui = About_Dialog()
+
+        # Kutsutaan käyttöliittymän muodostusmetodia setupUi
+        self.ui.setupUi(self)
 
     
+
 
 if __name__ == "__main__":
     
