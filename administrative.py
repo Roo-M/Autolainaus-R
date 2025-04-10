8a94aaee4f373a7a7211a004308f5399f638a247
Roo-M <51594@edu.raseko.fi>
2025-04-10 16:04:47 +0300

    Poistettu turhia print komentoja, lisätty lainaajan ja auton tallennukset, korjattu yhteydenottoa tietokantaan

--- administrative.py
+++ administrative.py
@@ -46,15 +46,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 # TODO: Mieti kannattaako muuttaa json.load(settingsFile)-komennoksi
                 jsonData = settingsFile.read()
                 self.currentSettings = json.loads(jsonData)
-
+            
+            # Puretaan salasana tietokantaoperaatioita varten
+            self.plainTextPassword = cipher.decryptString(self.currentSettings['password'])
             
             # Huom! Salasana pitää tallentaa JSON-tiedostoon tavallisena merkkijonona,
             # ei byte string muodossa. Salauskirjaston decode ja encode metodit hoitavat asian
             
-
         except Exception as e:
-            
-            print('Asetusten lataamisessa tapahtui virhe: ', str(e))
             self.openSettingsDialog()
 
 
@@ -67,7 +66,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Painikkeet
         self.ui.saveGroupPushButton.clicked.connect(self.saveGroup)
-
+        self.ui.savePersonPushButton.clicked.connect(self.savePerson)
+        self.ui.saveVehiclePushButton.clicked.connect(self.saveVehicle)
 
 
 
@@ -102,15 +102,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def saveGroup(self):
         # Määritellään tietokanta-asetukset
         dbSettings = self.currentSettings
-        plainTextPassword = cipher.decryptString(dbSettings['password'])
+        plainTextPassword = self.plainTextPassword
         dbSettings['password'] = plainTextPassword
-        print('Tietokanta asetukset ovat:', dbSettings)
 
         # Määritellään tallennusmetodin vaatimat parametrit
         tableName = 'ryhma'
         group = self.ui.groupNameLineEdit.text()
         responsiblePerson = self.ui.responsiblePersonLineEdit.text()
-        groupDictionary = {'ryhma': group,
+        groupDictionary = {
+                            'ryhma': group,
                            'vastuuhenkilo': responsiblePerson 
                            }
         
@@ -122,18 +122,99 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             dbConnection.addToTable(tableName, groupDictionary)
         except Exception as e:
             print('Virheilmoitus', str(e))
-            self.openWarning()
+            self.openWarning('Tallennus ei onnistunut.', str(e))
         
+    # Lainaajien tallennus
+    def savePerson(self):
+        # Määritellään tietokanta-asetukset
+        dbSettings = self.currentSettings
+        plainTextPassword = self.plainTextPassword
+        dbSettings['password'] = plainTextPassword
+
+        # Määritellään tallennusmetodin vaatimat parametrit
+        tableName = 'lainaaja'
+        ssn = self.ui.ssnLineEdit.text()
+        firstName = self.ui.firstNameLineEdit
+        lastName = self.ui.lastNameLineEdit.text()
+        # Asetetaan ryhmä tilapäisesti:
+        self.ui.groupComboBox.setCurrentText('Ei määritelty')
+
+        # TODO: Lisää tähän koodi, jolla haetaan ryhmät yhdistelmäruutuun
+        group = self.ui.groupComboBox.currentText()
+        vehicleClass = self.ui.vehicleClassLineEdit.text()
+        email = self.ui.emailLineEdit.text()
+        lenderDictionary = {
+                            'hetu': ssn,
+                           'etunimi': firstName,
+                           'sukunimi': lastName,
+                           'ryhma': group,
+                           'ajokorttiluokka': vehicleClass,
+                           'sahkoposti': email 
+                           }
+        
+        # Luodaan tietokantayhteys-olio
+        dbConnection = dbOperations.DbConnection(dbSettings)
+
+        # Kutsutaan tallennusmetodia
+        try:
+            dbConnection.addToTable(tableName, lenderDictionary)
+        except Exception as e:
+            print('Virheilmoitus', str(e))
+            self.openWarning('Tallennus ei onnistunut.', str(e))
+    
+
+
+    # Ajoneuvon tallennus
+    def saveVehicle(self):
+        # Määritellään tietokanta-asetukset
+        dbSettings = self.currentSettings
+        plainTextPassword = cipher.decryptString(dbSettings['password'])
+        dbSettings['password'] = plainTextPassword
+
+        # Määritellään tallennusmetodin vaatimat parametrit
+        tableName = 'auto'
+        licensePlateNumber = self.ui.licensePlateNumberLineEdit.text()
+        manufacturer = self.ui.manufacturerLineEdit.text()
+        model = self.ui.modelLineEdit.text()
+        modelYear = self.ui.modelYearLineEdit.text()
+        capacity = int(self.ui.capacityLineEdit.text())
+        
+        vehicleDictionary = {
+                            'rekisterinumero': licensePlateNumber,
+                             'merkki': manufacturer,
+                             'malli': model,
+                             'vuosimalli': modelYear,
+                             'henkilomaara': capacity
+                           }
+        
+        # Luodaan tietokantayhteys-olio
+        dbConnection = dbOperations.DbConnection(dbSettings)
+
+        # Kutsutaan tallennusmetodia
+        try:
+            dbConnection.addToTable(tableName, vehicleDictionary)
+        except Exception as e:
+            print('Virheilmoitus', str(e))
+            self.openWarning('Tallennus ei onnistunut.', str(e))
+
+
 
     # Virheilmoitukset ja muut Message Box -dialogit
     # ----------------------------------------------
 
     # Malli mahdollista virheilmoitusta varten
-    def openWarning(self):
+    def openWarning(self, title: str, text: str) -> None:
+        """Opens a message box for errors.
+
+        Args:
+            title (str): The title of the message
+            text (str): Error message
+        """
         msgBox = QtWidgets.QMessageBox()
         msgBox.setIcon(QtWidgets.QMessageBox.Critical)
-        msgBox.setWindowTitle('Kauheaa!')
-        msgBox.setText('Jotain kamalaa tapahtui')
+        msgBox.setWindowTitle(title)
+        msgBox.setText('Tapahtui vakava virhe')
+        msgBox.setDetailedText(text)
         msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msgBox.exec()
 
--- dbOperations.py
+++ dbOperations.py
@@ -30,19 +30,13 @@ class DbConnection():
         # Yhteysmerkkijono
         self.connectionString = f'dbname={self.databaseName} user={self.userName} password={self.password} host={self.server} port={self.port}'
 
-
-        print("Yhteysmerkkijono on:", self.connectionString)
-
     # Metodi tietojen lisäämiseen (INSERT)
-    def addToTable(self, table: str, data: dict) -> str:
+    def addToTable(self, table: str, data: dict) -> None:
         """Inserts a record (row) to a table according to a dictionary containing field names (columns) as keys and values
 
         Args:
             table (str): Name of the tables
             data (dict): Field names and values
-
-        Returns:
-            str: Information about successfull operation or an error message
         """
 
         # Muodostetaan lista sarakkeiden (kenttien) nimistä ja arvoista SQL lausetta varten
@@ -59,7 +53,7 @@ class DbConnection():
             if isinstance(rawValue, str):
                 value = f'\'{rawValue}\'' # \' mahdollistaa puolilainausmerkin lisäämisen
             else:
-                value =rawValue
+                value = f'{rawValue}'
             values += value + ', ' # Lisätään arvo sekä ja pilkku ja välilyönti
 
         # Poistetaan sarakkeista ja arvoista viimeinen pilkku ja välilyönti
@@ -79,7 +73,6 @@ class DbConnection():
 
             # Määritellään lopullinen SQL-lause
             sqlClause = f'INSERT INTO {table} ({columns}) VALUES ({values})'
-            print('SQL-lause on:', sqlClause)
 
             # Suoritetaan SQL-lause
             cursor.execute(sqlClause)
@@ -87,19 +80,21 @@ class DbConnection():
             # Vahvistetaan tapahtuma (transaction)
             currentConnection.commit()
 
+        # Jos tapahtuu virhe, välitetään se luokkaa käyttävälle ohjelmalle
         except (Exception, psycopg2.Error) as e:
-            message = 'Tietokantayhteyden muodostamisessa tapahtui virhe: ' + str(e)
+            raise e
 
         finally:
-            if message == '':
-                message = f'Tietue lisättiin tauluun {table}'
-
             # Selvitetään muodostuiko yhteysolio
             if currentConnection:
                 cursor.close() # Tuhotaan kursori
                 currentConnection.close() # Tuhotaan yhteys
 
-            return message
+    # TODO: Tee metodi tietojen lukemiseen
+
+    # TODO: Tee metodi tietojen muokkaamiseen
+
+    # TODO: Tee metodi tietueen poistamiseen
 
 if __name__ == "__main__":
 
